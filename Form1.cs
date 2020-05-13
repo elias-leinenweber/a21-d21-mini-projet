60c5a2de10dace9e7d0765d9702b8ae658b96ac3
Elias Leinenweber <elias.leinenweber@etu.unistra.fr>
2020-05-13 11:34:21 +0200

    modifié indentation

--- Form1.cs
+++ Form1.cs
@@ -1,18 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 
 namespace TorreDeBabel {
-	public partial class Form1 : Form {
-		public Form1()
-		{
-			InitializeComponent();
-		}
+public partial class Form1 : Form {
+	public Form1()
+	{
+		InitializeComponent();
 	}
+}
 }
